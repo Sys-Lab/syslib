7eb7d32c7b69ea6efaa6dd0b95cd3885f895af6c
kurokuro <kurokurokon@live.com>
2014-03-09 21:03:01 +0800

    update site;

--- site/site.js
+++ site/site.js
@@ -28,7 +28,7 @@ __Model.add("main",function(){
 			</div>
 		</div>
 		<div id="pre_build">
-			<div>Download Pre-Build Compressed Packs <span class="ver_small">Build 2014030801</span></div>
+			<div>Download Pre-Build Compressed Packs <span class="ver_small">Build 2014030903</span></div>
 			<div id="pre_build_dl">
 				<a target="_blank" href="dl/full/SYSLIB.min.js">
 					<div>Full</div>
